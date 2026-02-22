3e94c241e865c3e56da0085e012f6b4c157cce16
Flupinochan <flupino@metalmental.net>
2026-02-23 00:05:58 +0900

    画面全体の枠にpaddingを追加。横スクロールバーを追加

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,35 @@
 <template>
-  <CanvasPage />
+  <div class="app-scroll">
+    <div class="app-content">
+      <CanvasPage />
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
 import CanvasPage from './components/CanvasPage.vue'
 </script>
 
-<style scoped></style>
+<style>
+html,
+body,
+#app {
+  height: 100%;
+  margin: 0;
+  overflow: hidden;
+}
+</style>
+
+<style scoped>
+.app-scroll {
+  height: 100dvh;
+  overflow: auto;
+}
+
+.app-content {
+  padding: 10px;
+  box-sizing: border-box;
+  width: max-content;
+  min-width: 100%;
+}
+</style>
